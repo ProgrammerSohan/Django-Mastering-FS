4db3b7229076d7065c30c483b72f6c1422f4b0e1
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-07-26 23:43:16 +0600

    function based views-basics_c

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http.response import HttpResponse
 from django.urls import path,include 
+
+def home_view(request):
+    return HttpResponse("Home Page")
 
 urlpatterns = [
     #path('my_app/',include('my_app.urls')),
     path('admin/', admin.site.urls),
-    path('first_app/',include('first_app.urls'))
+    path('first_app/',include('first_app.urls')),
+    path('',home_view)
 ]
