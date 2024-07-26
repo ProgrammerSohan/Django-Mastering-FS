bdb2df173a712e495b94e177feb1241e1d48b715
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-07-26 22:53:21 +0600

    function based views-basics_a

--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http.response import HttpResponse
 # Create your views here.
+def simple_view(request):
+    return HttpResponse("Simple view by Sohan") #template html file (jinja)
