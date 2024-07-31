c152ae7c8f1a264f328e4c32e755003bac16e75f
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-07-31 23:50:14 +0600

    dynamic views_a

--- my_site/first_app/urls.py
+++ my_site/first_app/urls.py
@@ -3,6 +3,10 @@ from . import views
  
  #first_app/
 urlpatterns = [
-      path('',views.simple_view)
+    #  path('',views.simple_view)
     #  path('simple_view',views.simple_view)
+    path('sports/',views.sports_view),
+    path('finance/',views.finance_view)
+
+
    ]
--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 # Create your views here.
+""" 
 def simple_view(request):
     return HttpResponse("Simple view by Sohan") #template html file (jinja)
+"""
+def sports_view(request):
+    return HttpResponse("Sports Page")
+    
+def finance_view(request):
+    return HttpResponse("Finance Page")
+
+
+
