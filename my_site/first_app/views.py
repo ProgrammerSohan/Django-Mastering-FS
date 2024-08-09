668bf92ff279a0f8d6c238340ed5618055f51dfb
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-08-09 22:41:11 +0600

    ResponseNotFound & 404 pages_a

--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseNotFound
 # Create your views here.
 """ 
 def simple_view(request):
@@ -14,7 +14,13 @@ articles = {
 }
 
 def news_view(request,topic):
-    return HttpResponse(articles[topic])
+    try: 
+        result = articles[topic]
+        return HttpResponse(articles[topic])
+    except:
+        result = 'No page for that topic!'
+        return HttpResponseNotFound(result)
+
 
 def add_view(request,num1,num2):
     # domain.com/first_app/num1/num2 --> num1+num2
